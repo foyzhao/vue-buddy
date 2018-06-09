4ef1ec30e41616a546e7d5ccf1b4a5b7b3b11591
Foy <foyzhao@qq.com>
2018-06-10 00:30:14 +0800

    toast

---
+++ components/toast.vue
@@ -0,0 +1,53 @@
+<template>
+  <transition>
+    <div v-if="queue.length">
+      <p>{{queue[0]}}</p>
+    </div>
+  </transition>
+</template>
+
+<script>
+  let deferred = Promise.resolve();
+  export default {
+    data() {
+      return {
+        queue: []
+      }
+    },
+    methods: {
+      $toast(text, time = 2000) {
+        this.queue.push(text);
+        return deferred = deferred
+          .then(() => new Promise(done => setTimeout(done, time)))
+          .then(() => this.queue.shift());
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" scoped>
+  div {
+    position fixed
+    left 0
+    right 0
+    bottom 18%
+    z-index 9999
+    text-align center
+    transition .4s
+    &.v-enter
+    &.v-leave-to {
+      transform translateY(20px)
+      opacity 0
+    }
+    p {
+      display inline-block
+      margin 0 12%
+      padding .5rem .8rem
+      background #444
+      border-radius 2px
+      word-break break-all
+      font-size .8rem
+      color #fff
+    }
+  }
+</style>
--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 import KeepBack from './components/keep-back'
+import Toast from './components/toast.vue'
 
 function install(Vue) {
   Vue.component('KeepBack', KeepBack);
+
+  const toast = new (Vue.extend(Toast))().$mount();
+  document.body.appendChild(toast.$el);
+  Vue.prototype.$toast = toast.$toast;
 }
 
 export default {install}

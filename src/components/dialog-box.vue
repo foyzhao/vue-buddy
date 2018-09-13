8f8fd477e890e524e7c7a1b396482732cc59b012
foy <foyzhao@qq.com>
2018-09-13 13:52:07 +0800

    0.8.4 add dialog close button

--- src/components/dialog-box.vue
+++ src/components/dialog-box.vue
@@ -2,7 +2,7 @@
   <transition>
     <layer class="dialog-wrapper" @click="close">
       <div class="dialog-box">
-        <span class="close" v-if="$listeners.close" @click="$emit('close')"/>
+        <span class="close" v-if="$listeners.close" @click="$emit('close')">✖</span>
         <header class="header">
           <slot name="header"/>
         </header>
@@ -47,13 +47,23 @@
     }
   }
   .dialog-box {
+    position: relative;
     max-height: 100%;
     display: flex;
     flex-direction: column;
     background: #fff;
     transition: .2s;
     & > .close {
-      display: none;
+      position: absolute;
+      right: .2rem;
+      top: .2rem;
+      width: 1.5rem;
+      height: 1.5rem;
+      background: #00000088;
+      border-radius: 1.5rem;
+      text-align: center;
+      line-height: 1.5rem;
+      color: #fff;
     }
     & > .body {
       flex: 1 1 auto;

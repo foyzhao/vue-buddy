e8bcf91738efbc75f1ebd3c1c799fe3ee6ad3ef6
foy <foyzhao@qq.com>
2019-01-11 16:38:35 +0800

    0.10.2 center the loader

--- src/components/loader.vue
+++ src/components/loader.vue
@@ -1,12 +1,13 @@
 <template functional>
-  <span class="loader"/>
+  <span :class="['loader', {center: props.center !== undefined, inline: props.inline !== undefined}]"/>
 </template>
 
 <style>
   .loader {
-    display: inline-block;
-    width: 1rem;
-    height: 1rem;
+    display: block;
+    width: 1em;
+    height: 1em;
+    margin: 1rem auto;
     box-sizing: border-box;
     border-width: 2px;
     border-style: solid;
@@ -15,5 +16,20 @@
     border-right-color: #ffffff88;
     border-radius: 100%;
     animation: spin 1s infinite linear;
+  }
+  .loader.center {
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    width: 1.6em;
+    height: 1.6em;
+    margin: auto;
+  }
+  .loader.inline {
+    display: inline-block;
+    margin: 0;
+    vertical-align: middle;
   }
 </style>
